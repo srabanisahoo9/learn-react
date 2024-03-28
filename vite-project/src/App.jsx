c058f1a2fd466427022a1071591e4ca2e41da1d9
srabanisahoo9 <srabanis822@gmail.com>
2024-03-28 17:10:02 +0530

    inital commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,8 @@
+import TaskManager from "./TaskManager"
+
+function App(){
+
+  return(<TaskManager/>)
+}
+
+export default App
---
+++ vite-project/src/TaskManager.jsx
@@ -0,0 +1,82 @@
+import React, {useState} from 'react'
+
+function TaskManager(){
+     const [task, setTasks]=useState([]);
+    const[newTask, setNewTask]=useState("");
+
+    function handleInputChange(event){
+         setNewTask(event.target.value);
+    }
+    function addTask(){
+        if(newTask.trim()!==""){
+            setTasks(t=>[...t, newTask]);
+            setNewTask("");
+        }
+    } 
+    function deleteTask(index){
+         const updatedTasks=task.filter((_, i)=>i !== index);
+         setTasks(updatedTasks);
+    }
+    function moveTaskUp(index){
+         if(index>0){
+            const updatedTasks=[...task];
+            [updatedTasks[index], updatedTasks[index-1]]=[updatedTasks[index-1],updatedTasks[index]]
+
+            setTasks(updatedTasks);
+         }
+    }
+    function moveTaskDown(index){
+        if(index<task.length-1){
+            const updatedTasks=[...task];
+            [updatedTasks[index], updatedTasks[index-1]]=[updatedTasks[index-1],updatedTasks[index]]
+
+            setTasks(updatedTasks);
+         }
+
+    }
+    
+    return(<div className="todo-list">
+
+        <h1>TO DO LIST</h1>
+
+        <div>
+            <input
+                type="text"
+                placeholder="Enter a task.... "
+                value={newTask}
+                onChange={handleInputChange} />
+                <button
+                    className="add-button"
+                    onClick={addTask}>
+                    Add
+                </button>
+        </div>
+        <ol>
+            {task.map((task, index)=>
+            <li key={index}>
+                <span className="text">{task}</span>
+                <button
+                    className="delete-button"
+                    onClick={()=>deleteTask(index)}> 
+                    Delete    
+                </button>  
+
+                <button
+                    className="move-button"
+                    onClick={()=>moveTaskUp(index)}> 
+                    up   
+                </button> 
+
+                <button
+                    className="move-button"
+                    onClick={()=>moveTaskDown(index)}> 
+                    down  
+                </button>   
+            </li>
+            )}
+        </ol>
+
+    </div>);
+}
+
+export default TaskManager
